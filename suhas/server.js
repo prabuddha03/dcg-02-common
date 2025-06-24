53c55b0ee9f1c03e47ca0944a0a0f969cdc938ea
Suhas Halder <haldersuhas1@gmail.com>
2025-06-24 16:58:55 +0530

    initial

---
+++ suhas/server.js
@@ -0,0 +1,23 @@
+const express = require('express');    //requird for
+
+const app = express();
+const port = 3000;
+
+const suhas = {
+    name: 'suhas',
+    hometown: 'Fatepur',
+    degree: 'B.Tech',
+    email: 'suhas@gmail.com'
+}
+
+app.get('/me',(req, res)=>{
+    res.status(200).json({
+        status: 'success',
+        message: 'data featched successfully',
+        data: suhas
+    })
+})
+
+app.listen(3000, () => {
+    console.log(`erver is running on port ${port}`);
+});
