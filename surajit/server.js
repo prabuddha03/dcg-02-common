b7eee0c6bd982f9195ebfecbe51da13bd38614f0
crazysurajit123 <surajitsheet143@gmail.com>
2025-06-24 17:17:51 +0530

    initial commit

---
+++ surajit/server.js
@@ -0,0 +1,23 @@
+const express = require('express');//require is used to import the express module
+
+const app = express();//app is the express application
+const port = 3000;//port is the port number on which the server will run
+
+const surajit = {//prabuddha is the data object
+    name: 'surajit',
+    hometown: 'Durgapur',
+    degree: 'B.Tech',
+    email: 'surajit@gmail.com'
+}
+
+app.get('/me',(req, res)=>{//app.get is used to create a route
+    res.status(200).json({//res.status is used to set the status code
+        status: 'success',
+        message: 'Data fetched successfully',
+        data: surajit
+    })
+})
+
+app.listen(port, () => {//app.listen is used to start the server
+    console.log(`Server is running on port ${port}`);//console.log is used to print the message to the console
+});
