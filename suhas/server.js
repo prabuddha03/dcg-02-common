007ea10185ad0ecdb0c54ac8a8f3ea9b29acea1c
Suhas Halder <haldersuhas1@gmail.com>
2025-06-25 16:22:36 +0530

    basic get and post route created

--- suhas/server.js
+++ suhas/server.js
@@ -1,23 +1,54 @@
-const express = require('express');    //requird for
-
+const express = require('express');
+require('dotenv').config();
 const app = express();
-const port = 3000;
+app.use(express.json());                 // for post data from postmon
+
+const PORT = process.env.PORT || 3000;
 
 const suhas = {
-    name: 'suhas',
-    hometown: 'Fatepur',
-    degree: 'B.Tech',
-    email: 'suhas@gmail.com'
-}
+  name: 'suhas',
+  hometown: 'Fatepur',
+  degree: 'B.Tech',
+  email: 'suhas@gmail.com'
+};
+const products = [
+  {
+    name: 'p1',
+    productID: '1'
+  },
+  {
+    name: 'p2',
+    productID: '2'
+  }
+];
+app.get('/me', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'data fetched successfully',
+    data: suhas
+  });
+});
 
-app.get('/me',(req, res)=>{
-    res.status(200).json({
-        status: 'success',
-        message: 'data featched successfully',
-        data: suhas
-    })
-})
+app.get('/products', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'data fetched successfully',
+    data: products
+  });
+});
 
-app.listen(3000, () => {
-    console.log(`erver is running on port ${port}`);
+// create for post data from postmon
+app.post('/product', (req, res) => {
+  const product = req.body;
+  console.log(product);
+  res.status(201).json({
+    status: 'success',
+    message: 'product created successfully',
+    data: products
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
