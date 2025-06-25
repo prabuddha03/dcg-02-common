806c184027797de3a022e84ca519c8fd4cb44297
123sudipto <sudipto002gayen@gmail.com>
2025-06-25 16:22:00 +0530

    Basic get

--- sudipto/server.js
+++ sudipto/server.js
@@ -1,24 +1,78 @@
-const express = require('express');
-
+const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
+
+app.use(express.json());
 
 const sudipto = {
     name: "Sudipto",
     hometown: "Kolkata",
     degree: "B.Tech",
     email: "sudipto@gmail.com"
-
 }
 
+// const products = {
+//   firstproduct: {
+//     id: "001",
+//     name: "Product 1",
+//     price: "100",
+//   },
+
+//   secondproduct: {
+//     id: "002",
+//     name: "Product 2",
+//     price: "100",
+//   },
+
+//   thirdproduct: { id: "003", name: "Product 3", price: "100" },
+// };
+
+const products = [
+   {
+    id: "001",
+    name: "Product 1",
+    price: "100",
+  },
+
+   {
+    id: "002",
+    name: "Product 2",
+    price: "100",
+  },
+
+   { id: "003", 
+    name: "Product 3", 
+    price: "100" },
+];
+
 app.get ('/me', (req, res) =>{
-    res.status(200).json({
+    res.status(201).json({
         status: "success",
         message: "Data fetched successfully",
         data: sudipto
     })
-}) 
+})
 
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
+app.get("/product", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Data fetched successfully",
+    data: products,
+  });
+});
+
+app.post("/product",(req,res) =>{
+    const product = req.body;
+    console.log(product);
+    res.status(201).json({
+    status: "success",
+    message: "Product crreated successfully",
+    data: product,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  // console.log("Server is running on port " +port);
 });
