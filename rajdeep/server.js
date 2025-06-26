626c8090ffe5e87b90db25ee6f32aea51b3bef13
Rajdeep Mondal <rajdeepmondal909@gmail.com>
2025-06-26 13:30:50 +0530

    added basic get,put & delete request

--- rajdeep/server.js
+++ rajdeep/server.js
@@ -47,7 +47,10 @@ app.get('/products',(req, res) => {
 });
 
 app.post('/products',(req, res) => {
-    const myProduct = req.body;
+    const Product = req.body;
+
+    let a = myProduct.push(Product);
+    console.log(a);
     console.log(myProduct);
 
     res.status(201).json({
@@ -56,6 +59,44 @@ app.post('/products',(req, res) => {
         data: myProduct
     })
 });
+
+app.get('/products/:id',(req, res) => {
+    const {id} = req.params.id; // const {id} = req.params;
+    const product = myProduct[id];
+
+    res.status(200).json({
+        status:'sucess',
+        message:'data fetched successfully',
+        data: product
+    })
+});
+
+//for update the product
+app.put('/products/:id',(req, res)=>{
+    const newProduct = req.body;
+    const {id} = req.params;
+
+    myProduct[id] = newProduct;
+    console.log(myProduct);
+    res.status(200).json({
+        status:'sucess',
+        message:'data fetched successfully',
+        data: myProduct[id]
+    })
+})
+
+//for delete
+app.delete('/products/:id',(req, res)=>{
+    const {id} = req.params;
+    //myProduct.pop(myProduct[id]); 
+    myProduct.splice(id , 1);
+    console.log(myProduct);
+
+    res.status(204).json({
+        status:'sucess',
+        message:'data fetched successfully',
+    })
+})
 
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
