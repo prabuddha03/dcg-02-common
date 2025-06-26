35bffebc48df01c27de0739b7c2ac7c4c7370724
Your Name <arnabhaldar974@gmail.com>
2025-06-26 13:30:54 +0530

    added basic get,put,delete request

--- Arnab72/server.js
+++ Arnab72/server.js
@@ -41,7 +41,11 @@ app.get('/products',(req,res)=>{
 
 app.post('/products',(req, res)=>{
   const product = req.body;
-  console.log(product);
+
+  let a = allProducts.push(product);
+  console.log(a);
+  console.log(allProducts);
+
 
   res.status(201).json({
     status: 'success',
@@ -50,6 +54,45 @@ app.post('/products',(req, res)=>{
   })
 });
 
+app.get('/products/:id',(req, res)=>{
+  const {id} = req.params;//const {id}=req.params
+  const product= allProducts[id]; 
+  res.status(200).json({
+    status: 'success',
+    message: 'product fetched successfully',
+    data: product
+  })
+});
+
+app.put('/products/:id',(req , res)=>{
+  const newProduct = req.body;
+  const {id}= req.params;
+
+  allProducts[id] = newProduct;
+  console.log(allProducts);
+  res.status(200).json({
+    status: 'success',
+    message:'product edited successfully',
+    data: allProducts[id]
+  })
+
+})
+
+app.delete('/products/:id', (req, res)=>{
+  const {id}= req.params;
+  
+  allProducts.splice(id, 1);
+  //allProducts.pop(allProducts[id]);
+  console.log(allProducts);
+
+  res.status(204).json({
+    status: 'success',
+    message:'product deleted successfully',
+
+})
+})
+
 app.listen(PORT,() => {
   console.log(`server is running on port ${PORT}`);
 });
+
