a50322d994feacc24dc94b1cbdd27eb2bd12a966
Srijani1234 <sonaiadi2004@gmail.com>
2025-06-26 13:30:47 +0530

    added basic get,put and delete request

--- srijani/server.js
+++ srijani/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT;
 
-const allproduct=[
+const allProducts=[
     {
         name:'laptop',
         id:1,
@@ -22,9 +22,22 @@ const allproduct=[
         id:3,
 }
 ];
+app.get('/products',(req,res)=>{
+    res.status(200).json({
+    status:'success',
+    message:'Data fetched succesfully',
+    data:allProducts
+   })
+
+})
 app.post('/products',(req,res)=>{
     const product = req.body;
-    console.log(product);
+
+    let a = allProducts.push(product);
+    console.log(a);
+    console.log(allProducts);
+
+    allProducts.push(product);
 
 res.status(200).json({
     status:'success',
@@ -32,6 +45,45 @@ res.status(200).json({
     data:product
    })
 });
+
+app.get('/products/:id',(req,res)=>{
+    const { id } = req.params;
+    const product = allProducts[id];
+    res.status(200).json({
+        status: 'success',
+        message:'product fetched succesfully',
+        data:product
+    })
+
+});
+
+app.put('/products/:id',(req,res) =>{
+    const newProducts = req.body;
+    const {id} = req.params;
+
+    allProducts[id]=newProducts;
+    res.status(200).json({
+        status: 'success',
+        message:'product edited succesfully',
+        data:allProducts[id]
+
+    })
+
+})
+
+app.delete('/products/:id',(req,res)=>{
+    const { id } =req.params;
+    //allProducts.pop(allProducts[id]);//
+    allProducts.splice(id, 1);
+    console.log(allProducts);
+    res.status(200).json({
+        status: 'success',
+        message:'product deleted succesfully',
+        data:allProducts[id]
+
+    })
+
+})
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
