8d227d1a770cc4af4a96f01d569c4633ee0dc7e2
RakeshKayal <rakeshkayalleet276@gmail.com>
2025-06-30 12:12:47 +0530

    server init

---
+++ Rakesh/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+const myDetails = {
+    name: 'Prabuddha',
+    hometown: 'Durgapur',
+    degree: 'B.Tech',
+    email: 'prabuddha@gmail.com'
+}
+
+app.get('/me', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Data fetched successfully',
+        data: myDetails
+    })
+});
+
+const allProduts = [
+    {
+        name: 'Product 1',
+        price: 100,
+        quantity: 10
+    },
+    {
+        name: 'Product 2',
+        price: 200,
+        quantity: 20
+    },
+    {
+        name: 'Product 3',
+        price: 300,
+        quantity: 30
+    }
+    ]
+
+app.get('/products', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Data fetched successfully',
+        data: allProduts
+    })
+
+});
+
+app.post('/products', (req, res) => {
+    const product = req.body;
+    console.log(product);
+
+
+    res.status(201).json({
+        status: 'success',
+        message: 'Product created successfully',
+        data: product
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
