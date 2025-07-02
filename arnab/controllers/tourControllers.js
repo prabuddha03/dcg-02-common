3f3e45b43462bf8918cfe6d5a5a10d1cd26754ac
sardar-45 <arnabsardar102@gmail.com>
2025-07-02 22:30:39 +0530

    hello arnab

---
+++ arnab/controllers/tourControllers.js
@@ -0,0 +1,111 @@
+const Tour = require("../models/Tour");
+
+exports.createTour = async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body);
+        res.status(200).json(newTour);
+    } catch (error) {
+        console.log(error);
+        res.status(500),json(error);
+    }
+};
+
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: "sucess",
+            message: "Tours fetched sucessfully",
+            data: tours,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500),json(error);
+    }
+};
+
+exports.createTour = async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body);
+        res.status(200).json(newTour);
+    } catch (error) {
+        console.log(error);
+        res.status(500),json(error);
+    }
+};
+
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: "sucess",
+            message: "Tours fetched sucessfully",
+            data: tours,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500),json(error);
+    }
+};
+
+exports.getTourById = async (req, res) => {
+    try {
+        //tours/:id
+        const tour = await Tour.findById(req.params.id);
+        if (!tour) {
+            return res.status(404).json({
+                status: "error",
+                message: "Tour not found",
+            });
+        }
+        res.status(200).json({
+            status: "sucess",
+            message: "Tours fetched sucessfully",
+            data: tour,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500),json(error);
+    }
+};
+
+exports.updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if (!tour) {
+            return res.status(404).json({
+                status: "error",
+                message: "Tour not found",
+            });
+    }
+        res.status(200).json({
+            status: "sucess",
+            message: "Tours updated sucessfully",
+            data: tour,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500),json(error);
+    }
+};
+
+exports.deleteTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndDelete(req.params.id, req.body);
+        console.log(tour);
+        if (!tour) {
+            return res.status(404).json({
+                status: "error",
+                message: "Tour not found",
+            });
+        }
+        res.status(204).json({
+            status: "sucess",
+            message: "Tours deleted sucessfully",
+            data: tour,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500),json(error);
+    }
+};
--- arnab/server.js
+++ arnab/server.js
@@ -1,53 +1,144 @@
-const express = require('express');//require is used to import the express module
-require(`dotenv`).config();//dotenv is used to load environment variables from a .env file
+const express = require("express");
+const connectDB = require("./config/db");
+const tourRoutes = require("./routes/tourRoutes");
+const authRoutes = require("./routes/authRoutes")
+require("dotenv").config();
+
 const app = express();
+
 app.use(express.json());
 
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
-const player = [
-    {
-    name: 'rohit',
-    hometown:'mumbai', 
-    playfor: 'mumbai',
-    email:'rohitsharma45@icloud.com'
-},
-{
-    name: 'hardik',
-    hometown:'Mumbai', 
-    playfor: 'mumbai',
-    email:'hardikpandya33@icloud.com'
-},
+connectDB();
 
-{
-    name: 'virat',
-    hometown:'punjub', 
-    playfor: 'bangalor',
-    email:'virakholi18@icloud.com'
-},
-]
+const myDetails = {
+  name: "prabhat",
+  hometown: "shapoorji",
+  degree: "B.Tech",
+  email: "Prabhat@gmail.com",
+};
 
+app.get("/me", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Data fetched successfully",
+    data: myDetails,
+  });
+});
 
+const allProducts = [
+  {
+    name: "Product 1",
+    price: 100,
+    quantity: 10,
+  },
+  {
+    name: "Product 2",
+    price: 200,
+    quantity: 20,
+  },
+  {
+    name: "Product 3",
+    price: 300,
+    quantity: 30,
+  },
+];
 
-app.get('/players',(req, res)=>{
+const getAllProducts = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Data fetched successfully",
+    data: allProducts,
+  });
+};
+
+const createProduct = (req, res) => {
+  const product = req.body;
+  let a = allProducts.push(product);
+  console.log(a);
+  console.log(allProducts);
+
+  res.status(201).json({
+    status: "success",
+    message: "Product created successfully",
+    data: product,
+  });
+};
+
+const getProductById = (req, res) => {
+  const { id } = req.params;
+
+  if (id >= allProducts.length) {
+    res.status(404).json({
+      status: "error",
+      message: "product not found",
+    });
+    return;
+  } else {
+    const product = allProducts[id];
+    console.log(product);
     res.status(200).json({
-        status: 'success',
-        message: 'Data Fetched Succesfuly',
-        data: player
-    })
-})
-app.post('/players',(req, res)=>{
-    const newPlayer = req.body;
-    console.log(newPlayer);
-    res.status(201).json({
-        status: 'success',
-        message: 'Data Added Succesfuly',
-        data: player
-    })
-})
+      status: "success",
+      message: "Product fetched successfully",
+      data: product,
+    });
+  }
+};
 
+const updateProduct = (req, res) => {
+  const { id } = req.params;
 
+  if (id >= allProducts.length) {
+    res.status(404).json({
+      status: "error",
+      message: "product not found",
+    });
+    return;
+  }
+  const newProduct = req.body;
 
-app.listen(port, () => {//listen is used to start the server
-  console.log(`Server is running on ${port}`);//log the message to the console
+  allProducts[id] = newProduct;
+  console.log(allProducts);
+
+  res.status(200).json({
+    status: "success",
+    message: "product edited successfully",
+    data: allProducts[id],
+  });
+};
+
+const deleteProduct = (req, res) => {
+  const { id } = req.params;
+  if (id >= allProducts.length) {
+    res.status(404).json({
+      status: "error",
+      message: "product not found",
+    });
+    return;
+  }
+  allProducts.splice(id, 1);
+  console.log(allProducts);
+  res.status(204).json({
+    status: "success",
+    message: "product deleted successfully",
+  });
+};
+
+// Routes
+app
+    .route("/products")
+    .get(getAllProducts)
+    .post(createProduct);
+app
+  .route("/products/:id")
+  .get(getProductById)
+  .put(updateProduct)
+  .delete(deleteProduct);
+
+ 
+app.use ("/api/v1/tour",tourRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
